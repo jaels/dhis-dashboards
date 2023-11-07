e5822ccb399e7800c9caa92fbef77cc1d3ea2b3e
Yael Stein <yaelst@gmail.com>
2023-11-07 19:57:01 +0100

    add expanded details

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import { DashboardListItem } from './types/apiData'
-import { getDashboardList, getDashboardItemDetails } from './api/getData';
+import { getDashboardList, getDashboardItemDetails } from "./api/getData"
+import {  DashboardListItem, FullItemInfo } from './types/apiData'
 
 import DashboardCard from "./components/DashboardCard"
 
@@ -10,6 +10,9 @@ import DashboardCard from "./components/DashboardCard"
 const App: React.FC<{}> = () => {
 
   const [dashboardList, setDashboardList] = useState<DashboardListItem[] | null>(null)
+  const [expandedCardId, setExpandedCardId] = useState<string | null>(null)
+  const [expandedDetailsList, setExpandedDetailsList] = useState<FullItemInfo[]>([])
+
 
   const handleFetchList = async() => {
     const res = await getDashboardList()
@@ -31,17 +34,46 @@ const App: React.FC<{}> = () => {
   }
 
   useEffect(() => {
+    console.log(expandedDetailsList)
+  }, [expandedDetailsList])
+
+
+  useEffect(() => {
     handleFetchList()
   }, [])
 
-  
+  useEffect(() => {
+    if(dashboardList) {
+      setExpandedCardId(dashboardList[0].id)
+      handleFetchAllDetails()
+    }
+  }, [dashboardList])
+
+  const handleExpandCardClick = (id: string) => {
+    if (id !== expandedCardId) {
+      setExpandedCardId(id)
+    }
+    else setExpandedCardId(null)
+  }
+
+  const findExpandedDetailsById = (id: string) => {
+    return expandedDetailsList.find(obj => obj.id === id);
+  }
+
 
   return (
     <div className="App">
+      <div className="dashboards-header">
+        <h4>Dashboards</h4>
+      </div>
+      <hr></hr>
       <div className="cards-area">
         {dashboardList && dashboardList.map(item => (
           <DashboardCard
+            onExpandCardClick={(id) => handleExpandCardClick(id)} 
             itemInfo={item} 
+            expanded={item.id === expandedCardId} 
+            expandedDetails={findExpandedDetailsById(item.id)} 
             key={item.id}/>
         ))}
       </div>
--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -1,18 +1,61 @@
 import React, { useState, Fragment } from 'react';
 
-import { DashboardListItem } from '../types/apiData'
+import { DashboardListItem, FullItemInfo, DashboardItem } from '../types/apiData'
+
 
 /* @ts-ignore */
 import { Card } from '@dhis2-ui/card'
 import classnames from "classnames"
 
 interface Props {
+  expanded: boolean;
   itemInfo: DashboardListItem;
+  expandedDetails?: FullItemInfo | null | undefined;
+  onExpandCardClick: (id: string) => void;
 }
 
 const DashboardCard: React.FC<Props> = props => {
-  const { itemInfo } = props;  
+  const { expanded, itemInfo, expandedDetails, onExpandCardClick } = props;
+  
+  const findItemsRelevantText = (item: DashboardItem) => {
+    switch(item.type.toLowerCase()) {
+      case "visualization":
+        if(item.visualization)
+        return item.visualization.name
+        break;
+      case "map":
+        if(item.map)
+        return item.map.name
+        break;
+      case "text":
+        if(item.text)
+        return item.text
+        break;
+      default: 
+      return null
+    }
+  }
+  
+  const findItemsRelevantIcon = (item: DashboardItem) => {
+    switch(item.type.toLowerCase()) {
+      case "visualization":
+        if(item.visualization)
+        return "signal_cellular_alt"
+        break;
+      case "map":
+        if(item.map)
+        return "public"
+        break;
+      case "text":
+        if(item.text)
+        return "font_download"
+        break;
+      default: 
+      return null
+    }
+  }
 
+  
   return (
     <Card className="card-container">
       <div className="collapsed-card">
@@ -25,11 +68,29 @@ const DashboardCard: React.FC<Props> = props => {
           </span>
           <span 
             className={classnames("material-icons", "button-icons", "arrow-icon")} 
+            onClick={() => onExpandCardClick(itemInfo.id)}
           >
-            keyboard_arrow_down
+            {expanded ? "keyboard_arrow_up" : "keyboard_arrow_down"}
           </span>
         </div>
       </div>
+      {expanded && expandedDetails && (
+        <div className="expanded-card">
+          {expandedDetails.dashboardItems.map((item, i) => (
+            <Fragment key={i}>
+              {findItemsRelevantText(item) &&
+              <>
+              <div className="dashboard-item-info">
+                <span className="material-icons">{findItemsRelevantIcon(item)}</span>
+                <span className="dashboard-item-text">{findItemsRelevantText(item)}</span>
+              </div>
+                {i !== expandedDetails.dashboardItems.length - 1 && <hr></hr>}
+                </>
+              }
+            </Fragment>
+          ))}
+        </div>
+      )}
     </Card>
   )
 }
